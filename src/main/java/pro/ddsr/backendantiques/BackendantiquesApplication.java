365ee1b4af9b59c8fc54077b0d6818b301ebdbc1
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2024-09-20 15:13:05 -0500

    .

---
+++ src/main/java/pro/ddsr/backendantiques/BackendantiquesApplication.java
@@ -0,0 +1,13 @@
+package pro.ddsr.backendantiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendantiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendantiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/ddsr/backendantiques/BackendantiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ddsr.backendantiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendantiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
